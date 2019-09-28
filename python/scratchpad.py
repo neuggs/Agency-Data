5c896c8a2359d02b7709bf4fad69955c76410e8b
Frank Neugebauer <43097727+neuggs@users.noreply.github.com>
2019-09-28 13:43:30 -0400

    Completed dtc with tuning in Python.

--- python/scratchpad.py
+++ python/scratchpad.py
@@ -1,37 +1,20 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-import time
-from datetime import datetime
 import numpy as np
-import pprint
-
-from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 agency_df = pd.read_excel("../data/AgencyData_clean.xlsx")
-print(agency_df.shape)
+print("Original agency df shape:", agency_df.shape)
 
 # Trim the phat
-agency_df_used = agency_df.drop(['account_name', 'branch_name'], axis=1)
-print(agency_df_used.head(2))
-print(agency_df_used.shape)
+agency_df_pruned = agency_df.drop(['account_name', 'branch_name'], axis=1)
+print("Agency df pruned shape:", agency_df_pruned.shape)
 
-"""
-features = agency_df_used[[
-    "account_type",
-    "assigned_agent",
-    "lob",
-    "master_company",
-    "effective_date",
-    "policy_term",
-    "policy_type",
-    "annual_premium",
-    "written_premium",
-    "rating_state",
-    "status"
-]]
-"""
+# Need to remove values for transaction_type that have too few instances
+agency_df_used = agency_df_pruned.groupby('transaction_type').filter(lambda x : len(x)>3)
+print("Agency data used shape:", agency_df_used.shape)
 
 def ohe(df, feature):
     # encode
@@ -62,15 +45,46 @@ features = agency_df_used.loc[:, agency_df_used.columns != 'transaction_type']
 
 # Create training and test sets
 features_train, features_test, target_train, target_test = train_test_split(
-    features, target, test_size=0.1, random_state=1)
+    features, target, test_size=0.25, random_state=1)
 
-decision_tree_classifier = DecisionTreeClassifier(random_state=0)
-model = decision_tree_classifier.fit(features_train, target_train)
+# Set up cross validation validator
+criterion = ['gini', 'entropy']
+splitter = ['best', 'random']
+max_features = ['auto', 'sqrt']
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+min_samples_split = [2, 5, 10]
+min_samples_leaf = [1, 2, 4]
 
-y_predictions = model.predict(features_test)
-accuracy = accuracy_score(target_test, y_predictions)
-accuracy_converted = round((accuracy * 100), 2)
-print(accuracy_converted, "%")
+random_grid = {'criterion': criterion,
+               'splitter': splitter,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf}
+
+def randomSearchCV(the_grid):
+    decision_tree_classifier = DecisionTreeClassifier(random_state=0)
+    rtc_random = RandomizedSearchCV(estimator = decision_tree_classifier,
+                                    param_distributions = the_grid, n_iter = 100,
+                                    cv = 3, verbose=2, random_state=42, n_jobs = -1)
+    rtc_random.fit(features_train, target_train)
+
+    dt_class_bland = DecisionTreeClassifier(random_state=0)
+    dt_class_bland.fit(features_train, target_train)
+    y_predict = dt_class_bland.predict(features_test)
+    acc = accuracy_score(target_test, y_predict)
+    print("Bland accuracy score:", acc)
+    return rtc_random
+
+if __name__ == '__main__':
+    rtc = randomSearchCV(random_grid)
+    print("Best parameters:", rtc.best_params_)
+    print("Best score:", format(rtc.best_score_, '%'))
+    print("Error score:", rtc.error_score)
+    print("Scoring?", rtc.scoring)
+    the_predict = rtc.predict
+
 
 
 
